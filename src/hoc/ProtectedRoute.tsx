20666921512d11ad29517d1277b4023a32abd024
Igor Kolpakov <kolpakovigorjob@gragass.ru>
2023-07-10 20:30:41 +0300

    Пуш на 4 спринт

--- src/hoc/ProtectedRoute.tsx
+++ src/hoc/ProtectedRoute.tsx
@@ -1,30 +1,25 @@
-import {FC} from "react";
-import {Navigate, useLocation} from "react-router-dom";
-import {useAppSelector} from "../utils/hooks/reduxTypedHooks";
+import { FC } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAppSelector } from "../utils/hooks/reduxTypedHooks";
 
 
 interface ProtectedRouteProps {
     element: JSX.Element
-    anonymous?:boolean
+    anonymous?: boolean
 }
 
-export const ProtectedRoute: FC<ProtectedRouteProps> =({ element, anonymous = false }) => {
+export const ProtectedRoute: FC<ProtectedRouteProps> = ({ element, anonymous = false }) => {
     const isLogged = useAppSelector((store) => store.user.isLogged);
 
     const location = useLocation();
     const from = location.state?.from || '/';
-    // Если разрешен неавторизованный доступ, а пользователь авторизован...
     if (anonymous && isLogged) {
-        // ...то отправляем его на предыдущую страницу
-        return <Navigate to={ from } />;
+        return <Navigate to={from} />;
     }
 
-    // Если требуется авторизация, а пользователь не авторизован...
     if (!anonymous && !isLogged) {
-        // ...то отправляем его на страницу логин
-        return <Navigate to="/login" state={{ from: location}}/>;
+        return <Navigate to="/login" state={{ from: location }} />;
     }
 
-    // Если все ок, то рендерим внутреннее содержимое
     return element;
 }
